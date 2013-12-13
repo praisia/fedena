dacdaecb150b1ba4484751277444365f353a7250
Mohammad Faizan <mohammad.faizansaif@gmail.com>
2013-12-13 11:32:41 -0800

    thi

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'rails','2.3.5'
+gem 'i18n','0.4.2'
+gem 'declarative_authorization','0.5.1'
+gem 'mysql'
+gem 'rake','0.8.7'
+gem 'rush','0.6.8'
+gem 'ruby-debug'
+gem 'thin'
+
--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 Rails::Initializer.run do |config|
   config.time_zone = 'UTC'
   config.gem 'declarative_authorization', :source => 'http://gemcutter.org'
+  config.gem 'thin'
 
   config.load_once_paths += %W( #{RAILS_ROOT}/lib )
   config.load_paths += Dir["#{RAILS_ROOT}/app/models/*"].find_all { |f| File.stat(f).directory? }
